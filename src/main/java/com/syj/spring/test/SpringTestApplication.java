8b434e45ee682f9be0428b993211bc1d7e1c8e26
megaitsong <nana@naver.com>
2024-02-15 17:37:07 +0900

    [송여진] - springtest

---
+++ src/main/java/com/syj/spring/test/SpringTestApplication.java
@@ -0,0 +1,16 @@
+package com.syj.spring.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
+public class SpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestApplication.class, args);
+	}
+
+}
